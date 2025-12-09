5b62841cc20781a02a47c0bd326d0ef82b7b6583
h4pp2ach <jin4god@inu.ac.kr>
2025-12-09 11:09:41 +0900

    modify errors

--- taskManager.py
+++ taskManager.py
@@ -7,18 +7,18 @@ vel_HighAccidentZone = 50 # 사고다발지역 목표 속도
 vel_SpeedBump = 20 # 과속 방지턱 목표 속도
 vel_ObjInRoad = 10 # 도로 밖에서 장애물을 인식했을 때 목표 속도
 
-def getCurZoneTask(stop_event, 인자들):
+def getCurZoneTask(stop_event, args):
     while not stop_event.is_set():
-        globalVar.zoneInfo = getCurZone(인자)
+        globalVar.zoneInfo = getCurZone(args)
         sleep(0.1)
 
-def getObjInfoTask(stop_event, 인자들):
+def getObjInfoTask(stop_event, args):
     while not stop_event.is_set():
-        globalVar.isObjDetected, globalVar.isObjInRoad = getObjInfo(인자)
+        globalVar.isObjDetected, globalVar.isObjInRoad = getObjInfo(args)
         sleep(0.1)
 
 # 이게 메인 로직
-def mainTask(stop_event, 인자들):
+def mainTask(stop_event, arg):
     
     while not stop_event.is_set():
         zone = globalVar.zoneInfo
@@ -32,39 +32,39 @@ def mainTask(stop_event, 인자들):
                 speed = vel_ObjInRoad
         
         elif zone == zoneID['IDLE']:
-            continue
+            speed = globalVar.desiredSpeed
         elif zone == zoneID['CHILD']:
             speed = vel_ChildZone
         elif zone == zoneID['HIGHACCIDENT']:
             speed = vel_HighAccidentZone
-        elif zone == zoneID['CHILD']:
+        elif zone == zoneID['SPEEDBUMP']:
             speed = vel_SpeedBump
         
         globalVar.desiredSpeed = speed
         sleep(0.1)
 
 
-def getCurZone(인자):
+def getCurZone(arg):
     # 여기 짜면 돼
     zoneInfo = 0
     return zoneInfo
 
-def getObjDetected(인자):
+def getObjDetected(arg):
     # 여기 짜면 돼
-    isObjDetected = 0
+    isObjDetected = False
     return isObjDetected
 
-def getObjInRoad(인자):
+def getObjInRoad(arg):
     # 여기 짜면 돼
-    isObjInRoad = 0
+    isObjInRoad = False
     return isObjInRoad
 
-def getObjInfo(인자):
+def getObjInfo(arg):
     # 여기 짜면 돼
     
     # 센서퓨전
     
     
-    isObjDetected = getObjDetected(인자)
-    isObjInRoad = getObjInRoad(인자)
+    isObjDetected = getObjDetected(arg)
+    isObjInRoad = getObjInRoad(arg)
     return isObjDetected, isObjInRoad
