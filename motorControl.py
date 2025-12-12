81d342350121ff995647fead2bb8ba1e14221224
soojin-p <72116811+soojin-p@users.noreply.github.com>
2025-12-12 18:28:00 +0900

    Modify motor pin assignments and steering angle logic
    Updated motor control pin assignments and adjusted steering angle calculation.

--- motorControl.py
+++ motorControl.py
@@ -3,9 +3,9 @@ from time import sleep
 
 
 servoPWM = 17
-MotorENA = 13
-MotorIN1 = 6
-MotorIN2 = 5
+MotorENA = 4
+MotorIN1 = 21
+MotorIN2 = 26
 
 servo = AngularServo(servoPWM, min_angle=-90, max_angle=90,
                      min_pulse_width=0.0005, max_pulse_width=0.0024)
@@ -28,7 +28,7 @@ def lonControl(vel):
 def latControl(angle):
     angle = max(min(angle, 90), -90)
     
-    print(f"Steering Angle: {angle}")
-    servo.angle = angle
+    #print(f"Steering Angle: {angle}")
+    servo.angle = angle + 2
 
 
